d4453256da4677cb0072337b17212c10c29e926d
Mohammad Sadegh Zojaji <ms.zojaji@gmail.com>
2020-12-02 13:58:48 +0330

    Add the CloseButton element (T13672)

---
+++ Shared/CloseButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Zebble
+{
+    public class CloseButton : TextView
+    {
+        Func<Task> Action;
+
+        public CloseButton(Func<Task> action = null)
+        {
+            Css.Padding(8);
+            Css.Size(34);
+            Css.TextAlignment(Alignment.Middle);
+
+            Text = "✕";
+
+            Touched.Handle(OnTouched);
+
+            Action = action;
+        }
+
+        Task OnTouched() => Action?.Invoke();
+    }
+}
