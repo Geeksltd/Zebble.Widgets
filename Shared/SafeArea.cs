c8b5f3c979194a797c39c88d06436d10a8d5d1a2
Mohammad Sadegh Zojaji <ms.zojaji@gmail.com>
2020-12-02 18:16:36 +0330

    Add the SafeArea element (T13678)

---
+++ Shared/SafeArea.cs
@@ -0,0 +1,13 @@
+﻿using Zebble;
+
+namespace UI.Templates
+{
+    public class SafeArea : Canvas
+    {
+        public SafeArea()
+        {
+            Css.Height(Root.ActualHeight);
+            Css.Padding(top: Zebble.Device.Screen.SafeAreaInsets.Top);
+        }
+    }
+}
