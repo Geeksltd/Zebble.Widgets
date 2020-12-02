48ba471f4f6c6d097e3adb9450ecfd43c85387e7
Mohammad Sadegh Zojaji <ms.zojaji@gmail.com>
2020-12-02 16:29:16 +0330

    Add Button elements (T13675,T13676)

---
+++ Shared/Buttons.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zebble
+{
+    public class PrimaryButton : Button
+    {
+        public PrimaryButton(string text = null) => Text = text;
+
+        public Center InCenter()
+        {
+            var result = new Center();
+            result.Add(this).RunInParallel();
+            return result;
+        }
+    }
+
+    public class SecondaryButton : PrimaryButton
+    {
+        public SecondaryButton() => Css.BackgroundColor = "#686868";
+    }
+}
