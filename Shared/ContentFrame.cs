683acb87a946729eb33c60e36396795fdcffcf7a
Mohammad Sadegh Zojaji <ms.zojaji@gmail.com>
2020-12-02 13:45:12 +0330

    Add the ContentFrame element (T13674)

---
+++ Shared/ContentFrame.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace Zebble
+{
+    public class ContentFrame : Stack
+    {
+        public ContentFrame() { }
+
+        public Shadow Shadow { get; set; } = Shadow.Default;
+
+        public override async Task OnInitializing()
+        {
+            await base.OnInitializing();
+
+            Css.Background(color: Colors.LightGray);
+            Css.Margin(all: 15);
+            Css.Padding(all: 15);
+
+            this.BoxShadow(Shadow.X, Shadow.Y, Shadow.BlurRadius, Shadow.Expand, Shadow.Color);
+        }
+    }
+}
